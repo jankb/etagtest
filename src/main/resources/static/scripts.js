e61a4b68023484cee44c2902874b267014f5770e
Jan Kristian Bueie <jan.kristian.bueie@mattilsynet.no>
2023-10-04 21:40:01 +0200

    Bruker fetch for å hente data og populere tabell.

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,11 +1,39 @@
 
-function fetchData() {
+document.getElementById('getDataButton').addEventListener('click', fetchData);
+
+function fetchData(myform) {
    fetch("http://localhost:8181/country", {
    })
-   .then(function (response) {
+   .then(response =>  {
+    if (!response.ok) {
+        throw new Error('Network response was not ok: ' + response.statusText)
+    }
     return response.json();
    })
-   .then(function (data) {
-    console.log(data);
+   .then(data => {
+    populateTable(data.countries);
+    console.log(data.countries);
    })
+   .catch(error => console.error('Error fetching data: ', error));
+}
+
+function populateTable(countries){
+    const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
+
+   tableBody.innerHTML = "";
+
+    countries.forEach(country => {
+    console.log(country);
+        const newRow = tableBody.insertRow();
+
+         const idCell = newRow.insertCell(0);
+         const nameCell = newRow.insertCell(1);
+         const populationCell = newRow.insertCell(2);
+
+         idCell.innerHTML = `<input type="text" readonly value="${country.id}">`;
+         nameCell.innerHTML = `<input type="text" value="${country.name}">`;
+         populationCell.innerHTML = `<input type="text" value="${country.population}">`;
+
+    });
+
 }
