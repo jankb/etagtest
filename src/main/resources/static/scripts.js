dc18f90fbfda488c1778e5c6c04b2186364a8097
Jan Kristian Bueie <jan.kristian.bueie@mattilsynet.no>
2023-10-05 12:41:37 +0200

    Kan legge til linjer i html tabell.

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,39 +1,53 @@
 
 document.getElementById('getDataButton').addEventListener('click', fetchData);
+document.getElementById('addLineButton').addEventListener('click', addEmptyLine);
 
 function fetchData(myform) {
-   fetch("http://localhost:8181/country", {
-   })
-   .then(response =>  {
-    if (!response.ok) {
-        throw new Error('Network response was not ok: ' + response.statusText)
-    }
-    return response.json();
-   })
-   .then(data => {
-    populateTable(data.countries);
-    console.log(data.countries);
-   })
-   .catch(error => console.error('Error fetching data: ', error));
+  fetch("http://localhost:8181/country", {
+      })
+  .then(response =>  {
+      if (!response.ok) {
+      throw new Error('Network response was not ok: ' + response.statusText)
+      }
+      return response.json();
+      })
+  .then(data => {
+      populateTable(data.countries);
+      console.log(data.countries);
+      })
+  .catch(error => console.error('Error fetching data: ', error));
 }
 
 function populateTable(countries){
-    const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
+  const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
 
-   tableBody.innerHTML = "";
+  tableBody.innerHTML = "";
 
-    countries.forEach(country => {
-    console.log(country);
-        const newRow = tableBody.insertRow();
+  countries.forEach(country => {
+      console.log(country);
+      const newRow = tableBody.insertRow();
 
-         const idCell = newRow.insertCell(0);
-         const nameCell = newRow.insertCell(1);
-         const populationCell = newRow.insertCell(2);
+      const idCell = newRow.insertCell(0);
+      const nameCell = newRow.insertCell(1);
+      const populationCell = newRow.insertCell(2);
 
-         idCell.innerHTML = `<input type="text" readonly value="${country.id}">`;
-         nameCell.innerHTML = `<input type="text" value="${country.name}">`;
-         populationCell.innerHTML = `<input type="text" value="${country.population}">`;
+      idCell.innerHTML = `<input type="text" readonly value="${country.id}">`;
+      nameCell.innerHTML = `<input type="text" value="${country.name}">`;
+      populationCell.innerHTML = `<input type="text" value="${country.population}">`;
+      }
+      );
+}
 
-    });
+function addEmptyLine() {
+  const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
+  const newRow = tableBody.insertRow();
+
+  const idCell = newRow.insertCell(0);
+  const nameCell = newRow.insertCell(1);
+  const populationCell = newRow.insertCell(2);
+
+  idCell.innerHTML = `<input type="text" readonly value="">`;
+  nameCell.innerHTML = `<input type="text" value="">`;
+  populationCell.innerHTML = `<input type="text" value="">`;
 
 }
